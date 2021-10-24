1c430cc51d709e42640d7481fbd7cab54596e289
srishanth123 <93067302+srishanth123@users.noreply.github.com>
2021-10-24 10:36:45 +0530

    Add files via upload

---
+++ matrix add.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int search(int arr[], int n,int x)
+{
+	int i;
+	printf("\n enter the element to be searched");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	if (arr[i] == x)
+	   return i;
+	return -1;
+}
+int main(void)
+{
+	int arr[] = {2,3,4,10,40};
+	int x=10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = search(arr , n , x);
+    (result == -1);
+           printf("The element is not present in the array:");
+           printf("The element is present at index",result);
+          
+       return 0;
+}
+
